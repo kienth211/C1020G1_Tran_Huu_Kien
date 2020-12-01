d89529ef19a57cf29b69ac3e07633f91d5d7b8db
C1020G1_Tran_Huu_Kien <kienthd5h3@gmail.com>
2020-12-01 17:25:46 +0700

    update bai 3

---
+++ Module2/src/bai4_Class/Bai_tap/e2_stop_watch/StopWatch.java
@@ -0,0 +1,31 @@
+package bai4_Class.Bai_tap.e2_stop_watch;
+import java.time.LocalTime;
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+    }
+
+    public StopWatch(long startTime, long endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+    
+}
